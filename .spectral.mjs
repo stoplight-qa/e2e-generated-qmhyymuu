3d17aba2789dea50118d5d1c05fd4dfeaf91e380
qa-vtcfp-13-xdqk@example.com <qa-vtcfp-13-xdqk@example.com>
2023-04-26 17:22:35 +0000

    First commit of promoted project

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://x-16190.stoplight-dev.com/api/v1/projects/cHJqOjU5Mg/spectral.js?branch_slug=main&token=5e844e07-e584-4e6e-bb13-57c90a7b747b";
+export default { extends: ruleset };
